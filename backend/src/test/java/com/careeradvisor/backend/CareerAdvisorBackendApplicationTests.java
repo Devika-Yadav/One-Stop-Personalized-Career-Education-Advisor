9dd03fca8192fcb4c12d027cff19cb0317ec7f99
Devika-Yadav <devikayadavavula@gmail.com>
2026-01-04 11:10:34 +0530

    added springboot backend

---
+++ backend/src/test/java/com/careeradvisor/backend/CareerAdvisorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careeradvisor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerAdvisorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
